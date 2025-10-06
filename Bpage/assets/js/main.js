0cee8422429d5e4d57b8f4539e76ba02d6a54e70
macbook pro <macbookpro@optimist.local>
2025-10-05 23:20:42 -0700

    add: blog page added

---
+++ Bpage/assets/js/main.js
@@ -0,0 +1,10 @@
+// // Main JS for Optimist Luxe
+// // Mobile menu toggle
+// document.addEventListener('DOMContentLoaded', () => {
+//   const toggle = document.getElementById('nav-toggle');
+//   const navMenu = document.getElementById('nav-menu');
+
+//   toggle.addEventListener('click', () => {
+//     navMenu.classList.toggle('active');
+//   });
+// });
